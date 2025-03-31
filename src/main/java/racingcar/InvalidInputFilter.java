90bd508f00721693dc046c27764ab30eccb5b681
Walk1277 <jisu1445@gmail.com>
2025-03-31 16:58:29 +0900

    자동차 이름 검사 메서드 피드백 반영

--- src/main/java/racingcar/InvalidInputFilter.java
+++ src/main/java/racingcar/InvalidInputFilter.java
@@ -1,17 +1,13 @@
 package racingcar;
 
 public class InvalidInputFilter {
-    private static final int MAX_INT = 2147483647;
-    private static final int MIN_INT = -2147483648;
+    private static final int MAX_INT = 5;
+    private static final int MIN_INT = 1;
     public static void isCorrectNames(String[] CarNamesArr) {
-        int max = MIN_INT;
-        int min = MAX_INT;
         for (int i = 0; i < CarNamesArr.length; i++) {
-            if(CarNamesArr[i].length() > max) max = CarNamesArr[i].length();
-            if(CarNamesArr[i].length() < min) min = CarNamesArr[i].length();
-        }
-        if (max > 5 || min < 1 || min > max) {
-            throw new IllegalArgumentException("각 자동차 이름의 길이는 1~5 범위 내로 입력하세요.");
+            if(CarNamesArr[i].length() > MAX_INT || CarNamesArr[i].length() < MIN_INT) {
+                throw new IllegalArgumentException("각 자동차 이름의 길이는 1~5 범위 내로 입력하세요.");
+            }
         }
     }
 

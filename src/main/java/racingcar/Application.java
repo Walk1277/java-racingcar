6a9b198b91eac71ccb0c0e2ad8c66aae2f1c4ba2
Walk1277 <jisu1445@gmail.com>
2025-03-31 15:58:50 +0900

    우승자 출력 메서드 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,51 +5,20 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application { // 퍼블릭 키워드가 붙은 클래스는 파일명과 동일한 클래스명으로 작성
     public static void main(String[] args) {
         System.out.println("경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        // 자동차 이름 입력 함수
-        String[] CarNamesArr = inputCarNames(Console.readLine());
-        // 표준 예외 처리
-        InvalidInputFilter.isCorrectNames(CarNamesArr);
-
-        // 입력한 자동차 이름 개수만큼 이동 거리 정보를 담을 배열 생성
-        int[] distance = new int[CarNamesArr.length];
+        String[] CarNamesArr = inputCarNames(Console.readLine()); // 자동차 이름 입력 메서드
+        InvalidInputFilter.isCorrectNames(CarNamesArr); // 표준 예외 처리
+        int[] distance = new int[CarNamesArr.length]; // 입력한 자동차 이름 개수만큼 이동 거리 정보를 담을 배열 생성
 
         System.out.println("시도할 횟수는 몇 회인가요?");
-        // 시도 횟수 입력
-        String tryTimeStr = Console.readLine();
-        // 표준 예외 처리
-        InvalidInputFilter.isCorrectNumber(tryTimeStr);
-        // 시도 횟수를 int 형으로 변환하여 저장
-        int tryTime = Integer.parseInt(tryTimeStr);
+        String tryTimeStr = Console.readLine(); // 시도 횟수 입력
+        InvalidInputFilter.isCorrectNumber(tryTimeStr); // 표준 예외 처리
+        int tryTime = Integer.parseInt(tryTimeStr); // 시도 횟수를 int 형으로 변환하여 저장
 
-        // 경주 시작 함수
-        Racing.startRacing(CarNamesArr, distance, tryTime);
-
-        // 경기 결과 출력 함수
-        printWinner(CarNamesArr, distance);
+        Racing.startRacing(CarNamesArr, distance, tryTime); // 경주 시작 메서드
+        PrintWinner.printWinner(CarNamesArr, distance); // 경기 결과 출력 메서드
     }
 
     public static String[] inputCarNames(String CarNames) {
         return CarNames.split(",");
-    }
-
-    public static void printWinner(String[] CarNamesArr, int[] distance) {
-        boolean isPrinted = false;
-        int max = maxDistance(distance);
-        System.out.print("최종 우승자 : ");
-        for (int i = 0; i < distance.length; i++) {
-            if(isPrinted && distance[i]==max) System.out.print(", ");
-            if(distance[i]==max) {
-                System.out.print(CarNamesArr[i]);
-                isPrinted = true;
-            }
-        }
-    }
-
-    public static int maxDistance(int[] distance) {
-        int max = 0;
-        for (int i = 0; i < distance.length; i++) {
-            if(distance[i] > max) max = distance[i];
-        }
-        return max;
     }
 }
--- src/main/java/racingcar/PrintWinner.java
+++ src/main/java/racingcar/PrintWinner.java
@@ -1,4 +1,26 @@
 package racingcar;
 
 public class PrintWinner {
+    // 우승자 출력 메서드
+    public static void printWinner(String[] CarNamesArr, int[] distance) {
+        boolean isPrinted = false;
+        int max = maxDistance(distance);
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < distance.length; i++) {
+            if(isPrinted && distance[i]==max) System.out.print(", ");
+            if(distance[i]==max) {
+                System.out.print(CarNamesArr[i]);
+                isPrinted = true;
+            }
+        }
+    }
+
+    // 이동한 최대 거리 반환 메소드
+    public static int maxDistance(int[] distance) {
+        int max = 0;
+        for (int i = 0; i < distance.length; i++) {
+            if(distance[i] > max) max = distance[i];
+        }
+        return max;
+    }
 }
